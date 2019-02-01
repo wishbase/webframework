09e608983fda16e010ce65f6b3cdf4d58fbad4a7
wishbase <wishbase@163.com>
2019-02-01 14:46:41 +0800

    'src'

---
+++ src/test/JeecgOneGUI.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.jeecgframework.codegenerate.window.CodeWindow;
+
+import java.sql.SQLOutput;
+
+
+/**
+ * 【单表模型】代码生成器入口
+ *
+ */
+public class JeecgOneGUI {
+
+	public static void main(String[] args) {
+		CodeWindow  codeWindow = new CodeWindow();
+		codeWindow.pack();
+	}
+}
